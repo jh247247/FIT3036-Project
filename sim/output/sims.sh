cc84cb99d3f891da750705474e7a2ee0a5f7c263
Jack Hosemans <jh247247@gmail.com>
2015-10-12 22:15:17 +1100

    These weren't included in the previous commit...

--- sim/output/sims.sh
+++ sim/output/sims.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 SIM_PATH="../DaisyWorld.py"
 SIM_DEFAULT_OPTIONS="-g"
 SIM_START_TEMP=-24
@@ -10,46 +12,27 @@ MAX_THREADS_BACKGROUND=4
 
 OUTPUT_EXTENSION=".csv"
 
-# invasive black daisies options
-INVASIVE_START=22.5
-INVASIVE_END=42.5
-INVASIVE_INCREMENT=0.5
-INVASIVE_OPTION="-b"
+r=$1
 
-BASE_OUTPUT_NAME="normal"
-INVASIVE_OUTPUT_NAME="invasive"
+max=$(expr $r + $MAX_THREADS_BACKGROUND - 1)
 
-function gen {
-    r=1
-    # base case, no invasive
-    while [ $r -le $AMOUNT_OF_RUNS ]; do
-        for j in $(seq 1 $MAX_THREADS_BACKGROUND); do
-            if [ $r -le $AMOUNT_OF_RUNS ]; then
-                echo "Run count: " $r
-		# dont you love hacks...
-                python3 $SIM_PATH $SIM_DEFAULT_OPTIONS -s $SIM_STOP_TICK \
-                        -t $SIM_START_TEMP -r $SIM_INITIAL_RAD \
-                        -d $SIM_DELTA_RAD $2 $3 > \
-                        $1"-"$r$OUTPUT_EXTENSION &
-                ((r++))
-            fi
-        done
-        wait
-    done
-}
+while [ $r -le $max ]; do
+    # make file name
+    fname=$2"-"$r$OUTPUT_EXTENSION
 
-function plot {
-    octave --eval "plotOutput('$1')"
-}
+    # check if 3rd arg is set, then invasive enabled...
+    if [ ! -z "$3" ]; then
+	opt=$3" "$2
+	echo opt
+    fi
 
-function genplot {
-    EPOCH=`date +%s`
-    gen $1"-"$EPOCH $2
-    plot $1"-"$EPOCH
-}
-
-genplot $BASE_OUTPUT_NAME
-
-for i in $(seq $INVASIVE_START $INVASIVE_INCREMENT $INVASIVE_END); do
-    genplot $INVASIVE_OUTPUT_NAME"-"$i "$INVASIVE_OPTION $i"
+    # if file doesn't exist, create it
+    if [ ! -f $fname ]; then
+        python3 $SIM_PATH $SIM_DEFAULT_OPTIONS -s $SIM_STOP_TICK \
+                -t $SIM_START_TEMP -r $SIM_INITIAL_RAD \
+                -d $SIM_DELTA_RAD -rs $r $opt \
+                > $fname &
+    fi
+    ((r++))
 done
+wait
