aed1af87b52b569f85245490cf9a88045fec5ecb
Jack Hosemans <jh247247@gmail.com>
2015-10-22 23:43:51 +1100

    Modified plots

--- sim/output/plotOutput.m
+++ sim/output/plotOutput.m
@@ -13,29 +13,42 @@ function plotOutput(prefix,amount)
     default_data(:,:,i) = load(default_csv(i).name);
   end
 
-  fig = figure('Visible','off')
-  hold on
 
-  col = varycolor(length(default_csv));
-  for i = 1:length(default_csv)
-    plot(default_data(:,2,i),default_data(:,1,i), 'Color', col(i,:))
-  end
-  title(strcat(prefix,' ',num2str(amount)))
+
+  con = confidence(prefix, amount);
+  index = round(linspace(1,length(con)));
+
+  avgData = mean(default_data,3);
+  defaultAvg = mean(con,2);
+
+  fig = figure('Visible','off')
+  errorbar(avgData(index,2),avgData(index,1),
+	   abs(con(index,1)-con(index,2))/2)
+  hold on
+  plot(avgData(index,2), avgData(index,1),'.k')
+  hold on
+  title(['Plot type: ' prefix ' Amount of runs: ' num2str(amount)])
   xlabel('Solar Luminosity')
   ylabel('Temperature (C)')
-  print(fig, strcat(prefix,'.png'),'-dpng')
+  plot(avgData(:,2), avgData(:,5),'r')
+  ##plot(defaultAvg(:,2), defaultAvg(:,6),'g')
+  print(fig, strcat(prefix,'-avg.png'),'-dpng','-r72')
 
   close all
 
-  defaultAvg = mean(default_data,3);
   fig = figure('Visible','off')
-  plot(defaultAvg(:,2), defaultAvg(:,1),'.k')
   hold on
-  title(strcat(prefix,' ',num2str(amount)))
+
+  title(['Plot type: ' prefix ' Amount of runs: ' num2str(amount)])
+  plot(avgData(index,2), avgData(index,11),'r') # iblack
+  #plot(avgData(index,2), avgData(index,10),'dr') # iwhite
+  plot(avgData(index,2), avgData(index,9),'k') # black
+  plot(avgData(index,2), avgData(index,8),'g') # white
+  plot(avgData(index,2), avgData(index,7),'b') # blank
   xlabel('Solar Luminosity')
-  ylabel('Temperature (C)')
-  plot(defaultAvg(:,2), defaultAvg(:,5),'r')
-  plot(defaultAvg(:,2), defaultAvg(:,6),'g')
-  print(fig, strcat(prefix,'-avg.png'),'-dpng')
+  ylabel('Amount of type')
+  print(fig, strcat(prefix,'.png'),'-dpng','-r72')
+
+
 
 endfunction
